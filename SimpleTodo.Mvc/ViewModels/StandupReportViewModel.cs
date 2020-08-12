df3cfac4b15887d4886e1a7895653102f7d8b985
Josh Bullock <jbullock@parkplacetech.com>
2020-08-12 15:13:10 -0400

    Add standup report feature

---
+++ SimpleTodo.Mvc/ViewModels/StandupReportViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SimpleTodo.Mvc.Models.Todos;
+
+namespace SimpleTodo.Mvc.ViewModels
+{
+    public class StandupReportViewModel
+    {
+        public List<Todo> YesterdaysTodos { get; set; }
+        public List<Todo> TodaysTodos { get; set; }
+    }
+}
